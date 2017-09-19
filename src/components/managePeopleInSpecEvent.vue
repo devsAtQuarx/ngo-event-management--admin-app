7d29bd3c99dbb88718e50ecc034250d3acc7cfab
shivam singh <shivamsingh@shivams-MacBook-Air.local>
2017-09-19 23:49:01 +0530

    lastStage bug fix

--- src/components/managePeopleInSpecEvent.vue
+++ src/components/managePeopleInSpecEvent.vue
@@ -120,18 +120,20 @@
       this.$store.state.db.db.ref('peopleInEvent/' + this.$route.params.id)
         .limitToLast(1)
         .on('value',function(snapshot){
-          //console.log(Object.keys(snapshot.val())[0])
-          //console.log(vm.$store.state.events.eventsArr[0].key)
-          if(Object.keys(snapshot.val())[0] == vm.$store.state.regUsers.userRegInEvent[0].key){
-            //console.log("eq")
-            //do nothing
-          }else{
-            //console.log("not eq")
-            let newEvent = snapshot.val()
-            newEvent[Object.keys(snapshot.val())[0]].key = Object.keys(snapshot.val())[0]
-            vm.$store.state.regUsers.userRegInEvent.splice(0,0,newEvent[Object.keys(snapshot.val())[0]])
-            vm.$store.state.regUsers.userRegInEventCount += 1
-            //toast
+          if(snapshot.val() != null) {
+            //console.log(Object.keys(snapshot.val())[0])
+            //console.log(vm.$store.state.events.eventsArr[0].key)
+            if (Object.keys(snapshot.val())[0] == vm.$store.state.regUsers.userRegInEvent[0].key) {
+              //console.log("eq")
+              //do nothing
+            } else {
+              //console.log("not eq")
+              let newEvent = snapshot.val()
+              newEvent[Object.keys(snapshot.val())[0]].key = Object.keys(snapshot.val())[0]
+              vm.$store.state.regUsers.userRegInEvent.splice(0, 0, newEvent[Object.keys(snapshot.val())[0]])
+              vm.$store.state.regUsers.userRegInEventCount += 1
+              //toast
+            }
           }
         })
     },
